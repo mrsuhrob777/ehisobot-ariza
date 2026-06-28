593c40ff65fa9a22bea43f8fc6b55a768ccb4507
mrsuhrob777 <mrsuhrob777@gmail.com>
2026-06-28 22:38:00 +0500

    hardcode GROUP_ID fallback in config

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-GROUP_ID = os.getenv("GROUP_ID")
-if GROUP_ID:
-    GROUP_ID = int(GROUP_ID)
+BOT_TOKEN = os.getenv("BOT_TOKEN") or "8891098308:AAHaVSxWlvTi1c8ku9hDBXpRbAvf6pPxoLM"
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7959327686"))
+GROUP_ID = int(os.getenv("GROUP_ID", "-1004333077883"))
